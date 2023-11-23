1429745759fcf264465d15b7733e769f7c0eedde
Jakepps <nagalevsiy@mail.ru>
2023-11-24 00:25:09 +0300

    мега кринж

--- Assets/Branches/Jakepps/Scripts/HighlightMove.cs
+++ Assets/Branches/Jakepps/Scripts/HighlightMove.cs
@@ -6,6 +6,14 @@ namespace FragileReflection
 {
     public class HighlightMove : MonoBehaviour
     {
-        
+        public float X_START;
+        public float X_SPACE_BETWEEN_ITEMS;
+
+        public void MoveToIndex(int index)
+        {
+            float newX = X_START + (X_SPACE_BETWEEN_ITEMS * index);
+            Vector3 newPosition = new Vector3(newX, transform.localPosition.y, transform.localPosition.z);
+            transform.localPosition = newPosition;
+        }
     }
 }
