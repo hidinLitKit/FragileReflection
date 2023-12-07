e37fb5dc1b2f6e2f6dad914d9167d02924b17ecd
Jakepps <nagalevsiy@mail.ru>
2023-12-07 22:18:01 +0300

    По приколу сделал фиктивную загрузку))
    аче)

--- Assets/Scripts/MenuUI/MainMenuScript.cs
+++ Assets/Scripts/MenuUI/MainMenuScript.cs
@@ -8,8 +8,18 @@ namespace FragileReflection
 {
     public class ManeMenuScritp : MonoBehaviour
     {
+        [Header("Меню")]
         [SerializeField] private GameObject _mainMenu;
         [SerializeField] private GameObject _optionsMenu;
+
+        [Header("Загрузка")]
+        [SerializeField] private GameObject _downloadBar;
+        [SerializeField] private RectTransform _objectToMove;
+        [SerializeField] private RectTransform _targetPositionBegin;
+        [SerializeField] private RectTransform _targetPositionMid;
+        [SerializeField] private RectTransform _targetPositionEnd;
+        private float moveDuration = 2f;
+        private float pauseDuration = 1f;
 
         public void Quit()
         {
@@ -22,6 +32,15 @@ namespace FragileReflection
 
         public void LoadLevel()
         {
+            StartCoroutine(LevelDownload());
+        }
+
+        private IEnumerator LevelDownload()
+        {
+            StartDownload();
+
+            yield return new WaitForSeconds(5f);
+
             SceneManager.LoadScene("Main");
         }
 
@@ -35,6 +54,36 @@ namespace FragileReflection
         {
             _optionsMenu.SetActive(false);
             _mainMenu.SetActive(true);
+        }
+
+        public void StartDownload()
+        {
+            _mainMenu.SetActive(false);
+            _downloadBar.SetActive(true);
+            StartCoroutine(DownloadFocus());
+        }
+
+        private IEnumerator DownloadFocus()
+        {
+            float elapsedTime = 0f;
+
+            while (elapsedTime < moveDuration)
+            {
+                _objectToMove.position = Vector3.Lerp(_targetPositionBegin.position, _targetPositionMid.position, elapsedTime / moveDuration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(pauseDuration);
+
+            elapsedTime = 0f;
+
+            while (elapsedTime < moveDuration)
+            {
+                _objectToMove.position = Vector3.Lerp(_targetPositionMid.position, _targetPositionEnd.position, elapsedTime / moveDuration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
         }
 
     }
