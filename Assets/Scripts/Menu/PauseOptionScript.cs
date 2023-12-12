2fd7b260bbd8807707ab33fb429c648cfa1b459f
Jakepps <96076243+Jakepps@users.noreply.github.com>
2023-12-12 16:10:45 +0300

    сделал настройки в паузе

---
+++ Assets/Scripts/Menu/PauseOptionScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.VersionControl.Asset;
+using UnityEngine.SceneManagement;
+
+namespace FragileReflection
+{
+    public class PauseOptionScript : MonoBehaviour
+    {
+        [Header("Настройки")]
+        [SerializeField] private GameObject _buttonOptions;
+        [SerializeField] private GameObject _screenOptions;
+        [SerializeField] private GameObject _volumeOptions;
+        //[SerializeField] private GameObject _controlOpt;
+
+        [SerializeField] private GameObject _mainPause;
+
+        public void GotoScreenOpt()
+        {
+            _buttonOptions.SetActive(false);
+            _screenOptions.SetActive(true);
+        }
+
+        public void GotoVolumeOpt()
+        {
+            _buttonOptions.SetActive(false);
+            _volumeOptions.SetActive(true);
+        }
+
+        public void GotoOptionIn()
+        {
+            _screenOptions.SetActive(false);
+            _volumeOptions.SetActive(false);
+            _buttonOptions.SetActive(true);
+        }
+
+        public void GotoPause()
+        {
+            _buttonOptions.SetActive(false);
+            _mainPause.SetActive(true);
+        }
+    }
+}
