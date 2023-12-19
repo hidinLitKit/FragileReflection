c5ab602a66ae4c5501acad385d1b853363d5f087
Jakepps <nagalevsiy@mail.ru>
2023-12-19 14:26:10 +0300

    переделал главное меню+сделал перемещение камеры
    требует фиксов

--- Assets/Scripts/Menu/MainMenuScript.cs
+++ Assets/Scripts/Menu/MainMenuScript.cs
@@ -17,7 +17,14 @@ namespace FragileReflection
         [SerializeField] private GameObject _screenOptions;
         [SerializeField] private GameObject _volumeOptions;
         //[SerializeField] private GameObject _controlOpt;
-        
+
+        [Header("Точки остановки")]
+        public GameObject[] cameraPoints;
+
+        private Transform playerCamera;
+        [Header("Скорость передвижения камеры")]
+        public float moveSpeed = 5f;
+
         public void Quit()
         {
 #if UNITY_EDITOR
@@ -27,35 +34,60 @@ namespace FragileReflection
 #endif
         }
 
+        void Start()
+        {
+            playerCamera = Camera.main.transform;
+            MoveCameraToPoint(0);
+        }
+
         public void GotoOption()
         {
+            MoveCameraToPoint(1);
             _mainMenu.SetActive(false);
             _optionsMenu.SetActive(true);
         }
 
         public void GotoScreenOpt()
         {
+            MoveCameraToPoint(2);
             _buttonOptions.SetActive(false);
             _screenOptions.SetActive(true);
         }
 
         public void GotoVolumeOpt()
         {
+            MoveCameraToPoint(2);
             _buttonOptions.SetActive(false);
             _volumeOptions.SetActive(true);
         }
         
         public void GotoMainMenu()
         {
+            MoveCameraToPoint(0);
             _optionsMenu.SetActive(false);
             _mainMenu.SetActive(true);
         }
 
         public void GotoOptionIn()
         {
+            MoveCameraToPoint(1);
             _screenOptions.SetActive(false);
             _volumeOptions.SetActive(false);
             _buttonOptions.SetActive(true);
+        }
+
+        void MoveCameraToPoint(int newIndex)
+        {
+            StartCoroutine(MoveCameraSmooth(cameraPoints[newIndex].transform.position));
+        }
+
+        IEnumerator MoveCameraSmooth(Vector3 targetPosition)
+        {
+            while (Vector3.Distance(playerCamera.position, targetPosition) > 0.01f)
+            {
+                playerCamera.position = Vector3.MoveTowards(playerCamera.position, targetPosition, moveSpeed * Time.deltaTime);
+                yield return null;
+            }
         }
     }
 }
