50209d0c460a09fbde55e8971336d0b4680973a3
Jakepps <nagalevsiy@mail.ru>
2023-12-23 12:36:50 +0300

    пока думаю как лучше сделать

--- Assets/Scripts/Menu/AudioForButton/AudioButton.cs
+++ Assets/Scripts/Menu/AudioForButton/AudioButton.cs
@@ -1,24 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace FragileReflection
 {
-    public class AudioButton : MonoBehaviour
+    public class AudioButton : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler
     {
         [SerializeField] private AudioSource mainFx;
         [SerializeField] private AudioClip hoverFx;
-        //[SerializeField] private AudioClip clickFx;
+        [SerializeField] private AudioClip clickFx;
+        [SerializeField] private AudioClip someFx;
 
         public void HoverSound()
         {
             mainFx.PlayOneShot(hoverFx);
         }
 
-        // public void ClickSound()
-        // {
-        //     mainFx.PlayOneShot(clickFx);
-        // }
+        public void ClickSound()
+        {
+            mainFx.PlayOneShot(clickFx);
+        }
+
+        public void PlaySound()
+        {
+            mainFx.PlayOneShot(someFx);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            HoverSound();
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            ClickSound();
+        }
 
     }
 }
