f2f630e96d9a8b59d00525f9f251a565f385dae9
Jakepps <nagalevsiy@mail.ru>
2023-12-24 13:48:22 +0300

    fix

--- Assets/Scripts/Menu/AudioForButton/AudioButton.cs
+++ Assets/Scripts/Menu/AudioForButton/AudioButton.cs
@@ -6,43 +6,31 @@ namespace FragileReflection
 {
     public class AudioButton : MonoBehaviour
     {
-        [SerializeField] private AudioSource mainFx;
-        [SerializeField] private AudioClip hoverFx;
-        [SerializeField] private AudioClip clickFx;
-        [SerializeField] private AudioClip someFx;
+        [SerializeField] private AudioSource _mainFx;
+        [SerializeField] private AudioClip _hoverFx;
 
-        private void Start()
-        {
-            Button[] buttons = GetComponentsInChildren<Button>();
+        //private void Start()
+        //{
+        //    Button[] buttons = GetComponentsInChildren<Button>();
 
-            foreach (Button button in buttons)
-            {
-                // Добавляем обработчик события наведения мыши
-                EventTrigger trigger = button.gameObject.AddComponent<EventTrigger>();
-                EventTrigger.Entry pointerEnter = new EventTrigger.Entry { eventID = EventTriggerType.PointerEnter };
-                pointerEnter.callback.AddListener((eventData) => { HoverSound(); });
-                trigger.triggers.Add(pointerEnter);
+        //    foreach (Button button in buttons)
+        //    {
+        //        // Добавляем обработчик события наведения мыши
+        //        EventTrigger trigger = button.gameObject.AddComponent<EventTrigger>();
+        //        EventTrigger.Entry pointerEnter = new EventTrigger.Entry { eventID = EventTriggerType.PointerEnter };
+        //        pointerEnter.callback.AddListener((eventData) => { HoverSound(); });
+        //        trigger.triggers.Add(pointerEnter);
 
-                // Добавляем обработчик события нажатия мыши
-                EventTrigger.Entry pointerClick = new EventTrigger.Entry { eventID = EventTriggerType.PointerClick };
-                pointerClick.callback.AddListener((eventData) => { ClickSound(); });
-                trigger.triggers.Add(pointerClick);
-            }
-        }
+        //        // Добавляем обработчик события нажатия мыши
+        //        EventTrigger.Entry pointerClick = new EventTrigger.Entry { eventID = EventTriggerType.PointerClick };
+        //        pointerClick.callback.AddListener((eventData) => { ClickSound(); });
+        //        trigger.triggers.Add(pointerClick);
+        //    }
+        //}
 
         public void HoverSound()
         {
-            mainFx.PlayOneShot(hoverFx);
-        }
-
-        public void ClickSound()
-        {
-            mainFx.PlayOneShot(clickFx);
-        }
-
-        public void PlaySound()
-        {
-            mainFx.PlayOneShot(someFx);
+            _mainFx.PlayOneShot(_hoverFx);
         }
     }
 }
